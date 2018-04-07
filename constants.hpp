b6b4c6e63f1cafd1313b668370e2391dc1b0aabb
Nir Friedman <quicknir@gmail.com>
2018-04-07 18:17:59 -0400

    New approach that prevents segfaulting

--- constants.hpp
+++ constants.hpp
@@ -2,13 +2,20 @@
 
 #include <string>
 
+template <class T, T&(*init_fun)()>
+struct eager_initializer {
+    static T& instance;
+};
+template <class T, T&(*init_fun)()>
+T& eager_initializer<T, init_fun>::instance = init_fun();
+
 inline std::string& GetFoo() {
   static std::string foo = "I'm a string";
   return foo;
 }
-static std::string& foo = GetFoo();
+inline std::string& foo() { return eager_initializer<std::string, &GetFoo>::instance; }
 
-__attribute__((noinline)) inline std::string MakeBar() { return foo + " too"; }
+__attribute__((noinline)) inline std::string MakeBar() { return foo() + " too"; }
 
 inline std::string& GetBar() {
   static std::string bar = MakeBar();
